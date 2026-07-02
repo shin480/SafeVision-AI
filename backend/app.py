a72c01e0f5d9c96f10b1003f6fa23352b7e54a0c
yena <asdfg5346@naver.com>
2026-07-02 15:44:43 +0900

    기본환경 구성

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from backend.util.db import get_engine
+from sqlalchemy import text
+
+from fastapi import FastAPI, Query, UploadFile, File
+from starlette.staticfiles import StaticFiles
+from starlette.middleware.sessions import SessionMiddleware
+
+app = FastAPI()
+app.mount("/view", StaticFiles(directory="frontend"))
+
+app.add_middleware(SessionMiddleware, secret_key="motmachugetjyo")
+
+# db 연결 테스트
+@app.get('/db')
+def db_test():
+    conn = None
+    try:
+        conn = get_engine()
+        sql = text("show tables")
+        result = conn.execute(sql).fetchall()
+
+        tables = [row[0] for row in result]
+
+        return {
+            "success": True,
+            "tables": tables
+        }
+    except Exception as e:
+        print(f"🚨 DB 연결 에러: {e}")
+        return {"success": False, "message": "서버 오류가 발생했습니다."}
+    finally:
+        if conn:
+            conn.close()
