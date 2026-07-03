bebaf5e23951949c1f6d5451216da9199c00d9bd
sua-data <suai8402@gmail.com>
2026-07-03 13:12:29 +0900

    Update project structure

--- static/js/cctv-manage.js
+++
@@ -1,9 +0,0 @@
-// CCTV 관리 페이지용 JS
-
-const addButton = document.querySelector(".button");
-
-if (addButton) {
-  addButton.addEventListener("click", () => {
-    alert("CCTV 추가 기능은 백엔드 연결 후 구현됩니다.");
-  });
-}
