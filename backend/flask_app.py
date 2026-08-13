9f000aee2889d5ff6ac7379e4ea4e5ccdeb8a1f9
shin480 <a01066289646@gmail.com>
2026-08-13 09:52:39 +0900

    dd

--- backend/flask_app.py
+++ backend/flask_app.py
@@ -215,5 +215,5 @@ def delete_danger_zone(zone_id):
     )
     return jsonify(response.json())
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", debug=True, port=5000)
